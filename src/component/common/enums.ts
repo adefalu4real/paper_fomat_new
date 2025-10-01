25837524d9bc3c0aa6568de76406dd5cfcb0d911
Adefalu97 <adefalu97@gmail.com>
2025-10-01 14:55:32 +0100

    Add missing enums.ts file

---
+++ src/component/common/enums.ts
@@ -0,0 +1,19 @@
+// common/enums.ts
+export enum PaperType {
+  Essay = "Essay",
+  ResearchPaper = "Research Paper",
+  JournalArticle = "Journal Article",
+  Thesis = "Thesis",
+  Report = "Report",
+}
+
+export enum FormatType {
+  BasicAPA = "Basic APA",
+  BasicMLA = "Basic MLA",
+  APA7 = "APA 7th Edition",
+  MLA9 = "MLA 9th Edition",
+  IEEE = "IEEE",
+  Springer = "Springer",
+  Chicago = "Chicago",
+  FPI = "FPI Style (Project/Journal)", // Add this line
+}
